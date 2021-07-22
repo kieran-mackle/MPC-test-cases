fd21976c54f22f3e188858dc86618bb007c958da
Kieran <kemackle98@gmail.com>
2021-07-23 09:54:33 +1000

    Added shaded regions on plots for constraints

--- Archive/ball/simple_ball.m
+++ Archive/ball/simple_ball.m
@@ -36,21 +36,21 @@ target_ref          = [5]';
 cost.output         = eye(1);
 cost.control        = eye(1);
 
-constraints.hard.rate   = [-1, 1];
-constraints.hard.input  = [-5, 5];
-constraints.hard.output = [-10, 10];
+constraints.hard.rate   = [-1.5, 1.5];
+constraints.hard.input  = [-1.5, 1.5];
+constraints.hard.output = [-2, 10];
 
-constraints.soft.rate   = [-10, 10];
-constraints.soft.input  = [-30, 30];
-constraints.soft.output = [-10, 10];
+constraints.soft.rate   = [-1, 1];
+constraints.soft.input  = [-1, 1];
+constraints.soft.output = [-0, 8];
 
 % ----------------------------------------------------------------------- %
 % Solve MPC QP Problem
 % ----------------------------------------------------------------------- %
-output = solve_mpc(initial, params, cost, constraints, functions, ...
-                   0, target_ref);
-% output = solve_slack_mpc(initial, params, cost, constraints, functions, ...
+% output = solve_mpc(initial, params, cost, constraints, functions, ...
 %                    0, target_ref);
+output = solve_slack_mpc(initial, params, cost, constraints, functions, ...
+                   0, target_ref);
 
 
 % ----------------------------------------------------------------------- %
@@ -67,7 +67,29 @@ figure(1);
 hold on;
 grid on;
 title('MPC input');
+ylabel('Input force (N)');
+xlabel('Time (s)');
 stairs(output.time, output.control)
+patch('vertices', [0, constraints.soft.input(1); 
+                   0, constraints.hard.input(1); 
+                   params.sim_time, constraints.hard.input(1); 
+                   params.sim_time, constraints.soft.input(1)], ...
+    'faces', [1, 2, 3, 4], ...
+    'FaceColor', 'r', ...
+    'FaceAlpha', 0.3, ...
+    'LineStyle', 'none');
+plot([0,params.sim_time], [constraints.hard.input(2), ...
+     constraints.hard.input(2)], 'r-', 'LineWidth', 2);
+patch('vertices', [0, constraints.soft.input(2); 
+                   0, constraints.hard.input(2); 
+                   params.sim_time, constraints.hard.input(2); 
+                   params.sim_time, constraints.soft.input(2)], ...
+    'faces', [1, 2, 3, 4], ...
+    'FaceColor', 'r', ...
+    'FaceAlpha', 0.3, ...
+    'LineStyle', 'none');
+plot([0,params.sim_time], [constraints.hard.input(2), ...
+     constraints.hard.input(2)], 'r-', 'LineWidth', 2);
 
 figure(2);
 sgtitle('Physical state trajectory');
@@ -83,10 +105,31 @@ plot(output.time, output.state(2:2:end), 'k-');
 ylabel('velocity (m)');
 xlabel('Time (s)');
 
+
 figure(3);
 hold on;
 title('Output');
 grid on;
 plot(output.time, output.Z);
+patch('vertices', [0, constraints.soft.output(1); 
+                   0, constraints.hard.output(1); 
+                   params.sim_time, constraints.hard.output(1); 
+                   params.sim_time, constraints.soft.output(1)], ...
+    'faces', [1, 2, 3, 4], ...
+    'FaceColor', 'r', ...
+    'FaceAlpha', 0.3, ...
+    'LineStyle', 'none');
+plot([0,params.sim_time], [constraints.hard.output(2), ...
+     constraints.hard.output(2)], 'r-', 'LineWidth', 2);
+patch('vertices', [0, constraints.soft.output(2); 
+                   0, constraints.hard.output(2); 
+                   params.sim_time, constraints.hard.output(2); 
+                   params.sim_time, constraints.soft.output(2)], ...
+    'faces', [1, 2, 3, 4], ...
+    'FaceColor', 'r', ...
+    'FaceAlpha', 0.3, ...
+    'LineStyle', 'none');
+plot([0,params.sim_time], [constraints.hard.output(2), ...
+     constraints.hard.output(2)], 'r-', 'LineWidth', 2);
 ylabel('x-position (m)');
 xlabel('Time (s)');
